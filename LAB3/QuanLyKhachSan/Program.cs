3928a88e1c90e0982558c6103b76704a82a7e1c1
Tran Van Hieu <qntvh2005@gmail.com>
2026-09-25 19:48:01 +0700

    LAB3

---
+++ LAB3/QuanLyKhachSan/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+using QuanLyKhachSan.Forms;
+namespace QuanLyKhachSan
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FrmMain());
+        }
+    }
+}
